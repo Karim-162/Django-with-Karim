3d25d6ab00641497d853be18fcbf4a2f0b392fe2
Karim-162 <shafayetkarim04@gmail.com>
2026-04-03 03:34:29 +0600

    my project

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    context={
+        'name':'shafayet',
+        'age':23,
+        'nationality':'bangladeshi',
+    }
+    #return HttpResponse('<h1>hi welcome</h1>')
+    #return render(request,'index.html',{'name':name})
+    return render(request,'index.html',context)
