f477d7404d59100b150c98d5ba3f195c245f2e50
Karim-162 <shafayetkarim04@gmail.com>
2026-04-05 09:31:14 +0600

    Update views.py

--- wordcounter/myapp/views.py
+++ wordcounter/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 def index(request):
     #return HttpResponse('<h1>hi welcome shafayet</h1>')
