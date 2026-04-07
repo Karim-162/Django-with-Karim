6229aa7905c504efd95eea86e06520954b66c5b3
Karim-162 <shafayetkarim04@gmail.com>
2026-04-07 19:41:31 +0600

    myproject

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Feature
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 # Create your views here.
@@ -37,3 +39,27 @@ def index(request):
 
     features = Feature.objects.all() 
     return render(request,'index.html',{'features': features})
+
+def register(request):
+    if request.method == 'POST':
+        username= request.POST['username']
+        email= request.POST['email']
+        password= request.POST['password']
+        password2= request.POST['password2']
+
+        if password == password2: 
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'email is already exist')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'username is already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,password=password, email=email)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request, 'password not matched')
+            return redirect('register')
+    else:
+        return render(request,'register.html')
